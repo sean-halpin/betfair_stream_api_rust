ecbd4060fa6ea299f5a9bc3ef1c9d383e3ec1e41
sean <pulpfictionost@gmail.com>
2021-02-13 14:58:44 +0000

    Project Layout

--- exchange_recorder/src/exchange_recorder/exchange_subscribe_recorder.rs
+++ exchange_recorder/src/exchange_recorder/exchange_subscribe_recorder.rs
@@ -1,10 +1,14 @@
+use crate::metrics::metrics_statics::INCOMING_MESSAGES;
 use crate::AppConfig;
+use mongodb::{bson::doc, Client};
 use native_tls::TlsConnector;
 use native_tls::TlsStream;
+use std::io::BufRead;
+use std::io::BufReader;
 use std::io::Write;
 use std::net::TcpStream;
 
-pub fn connect_betfair_tls_stream(cfg: &AppConfig) -> Result<TlsStream<TcpStream>, String> {
+fn connect_betfair_tls_stream(cfg: &AppConfig) -> Result<TlsStream<TcpStream>, String> {
     println!("TLS connect starting");
 
     let auth_msg = format!(
@@ -33,4 +37,28 @@ pub fn connect_betfair_tls_stream(cfg: &AppConfig) -> Result<TlsStream<TcpStream
     tls_stream.write_all(sub_msg.as_bytes()).unwrap();
 
     return Ok(tls_stream);
+}
+
+pub async fn subscribe(cfg: &AppConfig) {
+    let market_id = &cfg.market_id;
+    let stream = connect_betfair_tls_stream(&cfg).unwrap();
+
+    let client = Client::with_uri_str("mongodb://root:password123@0.0.0.0:27017/")
+        .await
+        .unwrap();
+    let db = client.database("betfair_exchange_db");
+    let coll = db.collection(&market_id);
+
+    let mut stream_reader = BufReader::new(stream);
+    let mut buf = String::new();
+    while stream_reader.read_line(&mut buf).unwrap_or(0) > 0 {
+        INCOMING_MESSAGES.inc();
+        println!("{}", &buf);
+        let result = match coll.insert_one(doc! { "payload": &buf}, None).await {
+            Ok(_) => Ok("Inserted a document into MongoDB"),
+            Err(e) => Err(e),
+        };
+        println!("{:#?}", result);
+        buf = "".to_string();
+    }
 }
--- exchange_recorder/src/main.rs
+++ exchange_recorder/src/main.rs
@@ -1,40 +1,13 @@
 #![feature(proc_macro_hygiene, decl_macro)]
 #[macro_use]
 extern crate rocket;
-mod betfair_tls_connect;
 mod config;
+mod exchange_recorder;
 mod metrics;
-use config::app_config_load::AppConfig;
+use crate::config::app_config_load::AppConfig;
+use crate::exchange_recorder::exchange_subscribe_recorder::subscribe;
+use crate::metrics::metrics_server::start_web_server;
 use envconfig::Envconfig;
-use metrics::metrics_server::start_web_server;
-use metrics::metrics_statics::INCOMING_MESSAGES;
-use mongodb::{bson::doc, Client};
-use std::io::BufRead;
-use std::io::BufReader;
-
-async fn subscribe_to_betfair_exchange(cfg: &AppConfig) {
-    let market_id = &cfg.market_id;
-    let stream = betfair_tls_connect::connect_betfair_tls_stream(&cfg).unwrap();
-
-    let client = Client::with_uri_str("mongodb://root:password123@0.0.0.0:27017/")
-        .await
-        .unwrap();
-    let db = client.database("betfair_exchange_db");
-    let coll = db.collection(&market_id);
-
-    let mut stream_reader = BufReader::new(stream);
-    let mut buf = String::new();
-    while stream_reader.read_line(&mut buf).unwrap_or(0) > 0 {
-        INCOMING_MESSAGES.inc();
-        println!("{}", &buf);
-        let result = match coll.insert_one(doc! { "payload": &buf}, None).await {
-            Ok(_) => Ok("Inserted a document into MongoDB"),
-            Err(e) => Err(e),
-        };
-        println!("{:#?}", result);
-        buf = "".to_string();
-    }
-}
 
 #[tokio::main]
 async fn main() {
@@ -45,5 +18,5 @@ async fn main() {
 
     let moved_config = config.clone();
     tokio::spawn(async move { start_web_server(&moved_config).await });
-    subscribe_to_betfair_exchange(&config).await;
+    subscribe(&config).await;
 }
